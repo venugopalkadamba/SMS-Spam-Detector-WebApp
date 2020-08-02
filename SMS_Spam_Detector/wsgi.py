4ef4522ab4df2ea6a6823ea8747228a1d4cc68a0
VENU GOPAL KADAMBA <venugopalkadamba@gmail.com>
2020-08-02 20:35:12 +0530

    Add files via upload

---
+++ SMS_Spam_Detector/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SMS_Spam_Detector project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMS_Spam_Detector.settings')
+
+application = get_wsgi_application()
